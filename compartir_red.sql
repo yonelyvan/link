7b9ed78d72a74365fff257819dbb02503e1b80ff
levesques <yonelyvan@gmail.com>
2014-10-06 03:38:53 +0200

    Update compartir_red.sql

--- compartir_red.sql
+++ compartir_red.sql
@@ -1,10 +1,11 @@
+--====================comandos CMD===============
 netsh wlan show drivers
-====================comandos===============
+
 netsh wlan set hostednetwork mode=allow ssid=redhl key=y0nelyvan
 --ssid nombre de red
 --key password
 netsh wlan start hostednetwork
-
+--=============================================
 --luego
 >menu>panel de control>redes e internet>centro de redes y recursos compartidos>cambiar configuracion de adaptador
 
