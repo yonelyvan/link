7dead95e75216488f29cd5855718e56b76dd7587
levesques <yonelyvan@gmail.com>
2014-10-06 03:35:19 +0200

    Update compartir_red.sql

--- compartir_red.sql
+++ compartir_red.sql
@@ -1,2 +1,15 @@
-link
-====
+netsh wlan show drivers
+====================comandos===============
+netsh wlan set hostednetwork mode=allow ssid=redhl key=y0nelyvan
+--ssid nombre de red
+--key password
+netsh wlan start hostednetwork
+
+--luego
+>menu>panel de control>redes e internet>centro de redes y recursos compartidos>cambiar configuracion de adaptador
+
+>eternet>propiedades>uso compartido> 
+(activar ) permitir que los usuarios ....
+conexein de red domestica
+nombre que contine al nombre de la nueva red creada 
+  
